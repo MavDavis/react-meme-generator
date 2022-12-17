2dcb6c0acfa5247ab299e08070bb819ac2da4a01
Mavdavis <ekwesimarvelous@gmail.com>
2022-12-17 17:48:27 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import "./App.css";
-import Interest from "./components/Interest";
-import Info from "./components/Info";
-import Footer from "./components/Footer";
-function App({title,paragraph}) {
+import Navbar from "./Componenents/Navbar";
+function App() {
   return (
     <div className="App">
-      <Info />
-      <Interest title={'About'} paragraph={'Meticulous web developer with over 3 years of frontend experience and passion for responsive and aesthetic design. Worked on over 12  large scale projects both independently and with teams of  5 - 30.'}/>
-
-      <Interest title={'Interest'} paragraph={'Blockchain learning, Seo, data structures, Artificial Intelligence and more as I grow.'}/>
-      <Footer />
+      <Navbar />
+      <div className="div1">
+  2        <input type="text" />
+        <input type="text" />
+      </div>
     </div>
   );
 }
---
+++ src/Componenents/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Logo from '../assets/Logo.png'
+export default function Navbar(){
+    return(<nav>
+        <img src={Logo} alt/>
+        <p>React Course - Project 3</p>
+    </nav>)
+}
