7caee20b734faf03c089d4f8c9bc6fc52a970b32
Mavdavis <ekwesimarvelous@gmail.com>
2022-12-18 20:29:05 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
 import Navbar from "./Componenents/Navbar";
+import Main from "./Componenents/Main";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="div1">
-        <input type="text" />
-        <input type="text" />
-      </div>
-      <div className="button">
-        <p>Get a new meme image 🖼</p>
-      </div>
-      <div className="img-div">
-        <img src="" alt=""/>
-      </div>
+      <Main />
     </div>
   );
 }
---
+++ src/Componenents/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+export default function Main() {
+  return (
+    <>
+      <div className="div1">
+        <input type="text" />
+        <input type="text" />
+      </div>
+      <div className="button">
+        <p>Get a new meme image 🖼</p>
+      </div>
+      <div className="img-div">
+        <img src="" alt="" />
+      </div>
+    </>
+  );
+}
