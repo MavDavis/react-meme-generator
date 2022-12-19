3d33c7aa30d8e586d8f2d256e509d494bfbe0b57
Mavdavis <ekwesimarvelous@gmail.com>
2022-12-19 01:04:06 +0100

    commit

--- src/Componenents/Main.js
+++ src/Componenents/Main.js
@@ -1,4 +1,17 @@
 import React from "react";
+let obj = ''
+
+function fetchMeme(){
+let random = Math.random()*100
+ let ran = Math.floor(random) ;
+  fetch('https://api.imgflip.com/get_memes')
+    .then(response => response.json())
+    .then(response => {
+      obj = (response.data.memes[ran])
+    console.log(obj);
+    })
+    .catch(err => console.error(err));
+}
 export default function Main() {
   return (
     <>
@@ -6,11 +19,11 @@ export default function Main() {
         <input type="text" />
         <input type="text" />
       </div>
-      <div className="button">
+      <div onClick={fetchMeme} className="button">
         <p>Get a new meme image 🖼</p>
       </div>
       <div className="img-div">
-        <img src="" alt="" />
+     { obj ?  <img src={obj.url} alt="img" /> : <img src='' alt="img" />}
       </div>
     </>
   );
--- src/Componenents/Navbar.js
+++ src/Componenents/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from '../assets/Logo.png'
 export default function Navbar(){
     return(<nav>
-        <img src={Logo} alt/>
+        <img src={Logo} alt=''/>
         <p>React Course - Project 3</p>
     </nav>)
 }
