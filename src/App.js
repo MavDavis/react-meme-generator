18eb7dd518f1b8968facd130c3392d879a9beaea
Mavdavis <ekwesimarvelous@gmail.com>
2022-12-20 15:14:17 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
 import Navbar from "./Componenents/Navbar";
 import Main from "./Componenents/Main";
+import { useState } from "react";
 function App() {
+ const [num, setNum] = useState(0);
+ const add = ()=>{
+  setNum(num + 1)
+ }
+ const subtract = ()=>{
+  setNum(num - 1)
+ }
   return (
     <div className="App">
       <Navbar />
       <Main />
+<div className="div">
+
+</div>
     </div>
   );
 }
--- src/Componenents/Main.js
+++ src/Componenents/Main.js
@@ -1,30 +1,64 @@
-import React from "react";
-let obj = ''
+import React, { useState } from "react";
+let obj = "";
 
-function fetchMeme(){
-let random = Math.random()*100
- let ran = Math.floor(random) ;
-  fetch('https://api.imgflip.com/get_memes')
-    .then(response => response.json())
-    .then(response => {
-      obj = (response.data.memes[ran])
-    console.log(obj);
-    })
-    .catch(err => console.error(err));
-}
 export default function Main() {
+  const [res, changeRes] = useState("");
+  const [upperInput, changeUpperInput] = useState("");
+  const [lowerInput, changeLowerInput] = useState("");
+//   const [thingsArray, setThingsArray] = useState(["thing1", "thing2"]);
+//  let pTag = thingsArray.map((item) => {
+//     return <p>{item}</p>;
+//   });
+  function fetchMeme() {
+    let random = Math.random() * 100;
+    let ran = Math.floor(random);
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        obj = response.data.memes[ran].url;
+        console.log(obj);
+        changeRes(obj);
+      })
+      .catch((err) => console.error(err));
+  }
+  function changeUpperInputValue(e) {
+    if (e.key === "Enter") {
+      changeUpperInput(e.target.value);
+      e.target.value = "";
+    }
+  }
+  function changeLowerInputValue(e) {
+    if (e.key === "Enter") {
+      changeLowerInput(e.target.value);
+      e.target.value = "";
+    }
+  }
+
   return (
     <>
       <div className="div1">
-        <input type="text" />
-        <input type="text" />
+        <input type="text" onKeyUp={changeUpperInputValue} />
+        <input type="text" onKeyUp={changeLowerInputValue} />
       </div>
       <div onClick={fetchMeme} className="button">
         <p>Get a new meme image 🖼</p>
       </div>
       <div className="img-div">
-     { obj ?  <img src={obj.url} alt="img" /> : <img src='' alt="img" />}
+        <p className="input top">{upperInput}</p>
+        {res.length ? <img src={res} alt="img" /> : ""}
+        <p className="input bottom">{lowerInput}</p>
       </div>
+      {/* <div>{pTag}</div>
+      <p
+        onClick={() => {
+          setThingsArray((oldValue) => [
+            ...oldValue,
+            `Things${oldValue.length + 1}`,
+          ]);
+        }}
+      >
+        addp
+      </p> */}
     </>
   );
 }
