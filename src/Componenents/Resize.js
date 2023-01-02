2db3ab06ed16943aaaeb4e49c0e6ca02a0717c0e
Mavdavis <ekwesimarvelous@gmail.com>
2023-01-02 10:58:07 +0100

    return for useEffect

--- src/Componenents/Resize.js
+++ src/Componenents/Resize.js
@@ -1,16 +1,20 @@
-import React, {useEffect, useState} from "react";
-export default function Resize(){
-    const [size, setSize] = useState(window.innerWidth)
+import React, { useEffect, useState } from "react";
+export default function Resize() {
+  const [size, setSize] = useState(window.innerWidth);
 
-    const toggleSize= ()=>{
-        setSize(window.innerWidth)
-                }
+  const toggleSize = () => {
+    setSize(window.innerWidth);
+  };
 
-    useEffect(()=>{
-        window.addEventListener('resize',toggleSize)
-return(window.removeEventListener('resize', toggleSize()))
-    },[])
-    return(<>
-    <p>windows inner-width: {size}</p>
-    </>)
+  useEffect(() => {
+    window.addEventListener("resize", toggleSize);
+    return () => {
+      window.removeEventListener("resize", toggleSize());
+    };
+  }, []);
+  return (
+    <>
+      <p>windows inner-width: {size}</p>
+    </>
+  );
 }
